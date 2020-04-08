b3c5c64252a4fc4c8c759d40f0b9c543e8f050b8
Tiffany-Lou <63366271+Tiffany-Lou@users.noreply.github.com>
2020-04-08 12:51:33 -0500

    Add files via upload

---
+++ HW1_EX1.cpp
@@ -0,0 +1,29 @@
+// Author: Tiffany Lourdraj
+// Course: CS1336
+// Date:  September 8th 2018
+// Assignment:  Lecture Homework #1 Exercise #1
+// Compiler:    Apple Xcode
+
+// Description: the purpose of this program is to print lines and a design
+//
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl ;
+    cout << "C++ Programming is fun!\n"  ;
+    cout << "The value of 2 times 3 is: " <<  (2*3) << endl;
+    cout << "*********      *         *" << endl;
+    cout << "*       *     ***      *   *" << endl;
+    cout << "*       *    *****   *       *"<< endl;
+    cout << "*       *      *    *         *"<< endl;
+    cout << "*       *      *   *           *"<< endl;
+    cout << "*       *      *    *         *"<< endl;
+    cout << "*       *      *     *       *"<< endl;
+    cout << "*       *      *       *   *"<< endl;
+    cout << "*********      *         *"<< endl;
+    
+} //end int main()
